57923117afe9b57b061c509fdf989d579f6f664a
arthur-feral <arthur.feral@hotmail.fr>
2016-02-28 23:37:36 +0100

    feat(config): load options from CLI or file

--- bin/reactplease.js
+++ bin/reactplease.js
@@ -8,14 +8,16 @@ const packagejson = require('../package.json');
 
 commander
   .version(packagejson.version)
-  .usage('[options] <react class name>')
-  .option('-i, --ignore', 'ignore .reactpleaserc file')
+  .usage('[options] [react class name]')
+  .option('-i, --import <packages>', 'import special packages top of class file')
+  .option('--ignore', 'ignore .reactpleaserc file')
+  .option('--es6', 'use EcmaScript 6 syntax')
   .parse(process.argv);
 
-//if (!commander.args.length) {
-//  commander.help();
-//} else {
-//}
-reactplease.process(commander);
+if (!commander.args.length) {
+  commander.help();
+} else {
+  reactplease.process(commander);
 
-process.exit(0);
+  process.exit(0);
+}
